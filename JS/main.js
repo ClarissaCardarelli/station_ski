748022e0a184c7e399e2f60f76e41a488352bfcc
Uldyssian <mickael.brouttier@gmail.com>
2025-10-10 15:28:31 +0200

    Ajustage micka

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,18 @@
-  const video = document.getElementById('myVideo');
   document.body.addEventListener('click', () => {
     video.muted = false;
   }, { once: true });
+
+  
+  const video = document.getElementById('myVideo');
+  const button = document.getElementById('audio-control')
+
+  button.addEventListener('click', () => {
+    if (video.muted) {
+      video.muted = false ;
+      button.textContent = 'Mute' ;
+    }
+    else {
+      video.muted = true ;
+      button.textContent = 'Démute' ;
+    }
+  })
